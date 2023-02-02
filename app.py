1bf3eb71cffcfb4253ae450fbd4a215e01f51d55
Justin <justinrkinsman@gmail.com>
2023-02-02 10:46:39 -0400

    Add additional python code

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print('Hello World')
+#first_number = input('Enter your first number: ')
+#second_number = input('Enter your second number: ')
+#print('Sum: ' + str(float(first_number) + float(second_number)))
+
+""" weight = input("Weight: ")
+units = input("(K)g or (L)bs: ")
+
+if units.upper() == "L":
+    print("Weight in Kg: " + str(int(weight) / 2.205))
+elif units.upper() == "K":
+    print("Weight in Lbs: " + str(int(weight) * 2.205)) """
+
+i = 1
+while i <= 10:
+    print(i * '*')
+    i = i + 1
