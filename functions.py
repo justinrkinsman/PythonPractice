fb8f98c5d336d99d222c2d8a440616d6d1a9c074
Justin <justinrkinsman@gmail.com>
2023-02-06 11:51:26 -0400

    Create functions using python

---
+++ functions.py
@@ -0,0 +1,15 @@
+def greet_user(name, last_name):
+    print(f"Hi {name} {last_name}!")
+    print("Welcome aboard")
+
+
+print("Start")
+greet_user("John", last_name="Smith") #keyword arguments should be used after positional arguments
+print("Finish")
+
+def square(number):
+    return number * number #All functions return the value "None by default"
+
+print(square(4))
+
+#Creating a Reusable Function 2:49:07
