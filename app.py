ca680364070973984826a53d655653c7e8607866
Justin <justinrkinsman@gmail.com>
2023-02-08 10:55:39 -0400

    Add pathlib

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-#first_number = input('Enter your first number: ')
-#second_number = input('Enter your second number: ')
-#print('Sum: ' + str(float(first_number) + float(second_number)))
+from pathlib import Path
 
-weight = input("Weight: ")
-units = input("(K)g or (L)bs: ")
-
-if units.upper() == "L":
-    print("Weight in Kg: " + str(int(weight) / 2.205))
-elif units.upper() == "K":
-    print("Weight in Lbs: " + str(int(weight) * 2.205))
+path = Path()
+for file in path.glob('*.py'):
+    print(file)
