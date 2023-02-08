23748e3842b45d8403ad8e337f045628a20730af
Justin <justinrkinsman@gmail.com>
2023-02-08 10:46:23 -0400

    Adjust function for dice roll

--- random_value.py
+++ random_value.py
@@ -1,10 +1,11 @@
 import random
 
 class Dice:
-    def roll():
+    def roll(self):
         number1 = random.randint(1, 6)
         number2 = random.randint(1, 6)
-        result = (number1, number2)
-        print(result)
+        return number1, number2 #automatically formats as tuple
 
-Dice.roll()
+dice = Dice()
+
+print(dice.roll())
